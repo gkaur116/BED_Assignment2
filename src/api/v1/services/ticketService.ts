a38b1bec21f3a7d5e52cdbfb8c4ac23f35d03774
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-06 20:34:54 -0600

    Added ticketservice.ts

---
+++ src/api/v1/services/ticketService.ts
@@ -0,0 +1,13 @@
+export type TicketPriority = "critical" | "high" | "medium" | "low";
+export type TicketStatus = "open" | "in-progress" | "resolved";
+
+// Support ticket details
+export interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+  priority: TicketPriority;
+  status: TicketStatus;
+  createdAt: string;
+  updatedAt: string;
+}
