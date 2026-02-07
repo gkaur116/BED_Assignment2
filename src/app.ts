ff25db726592afb7b8d72121cb1c9f41c817a756
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-06 20:19:54 -0600

    added health checkpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 
 const app: Express = express();
 
 app.use(morgan("combined"));
 app.use(express.json());
+app.use("/api/v1", healthRoutes);
 
 export default app;
