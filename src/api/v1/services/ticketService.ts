d6c59c2bdd17085c0bd61d6d0607a0c470e84c79
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-06 21:03:49 -0600

    Added create ticketservice function

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -1,3 +1,5 @@
+import { tickets } from "../../../data/ticket"
+
 export type TicketPriority = "critical" | "high" | "medium" | "low";
 export type TicketStatus = "open" | "in-progress" | "resolved";
 
@@ -11,3 +13,27 @@ export interface Ticket {
   createdAt: string;
   updatedAt: string;
 }
+
+export const createTicket = (
+  title: string,
+  description: string,
+  priority: TicketPriority
+): Ticket => {
+  const now = new Date().toISOString();
+
+  
+  const newTicket: Ticket = {
+    id: tickets.length + 1,
+    title,
+    description,
+    priority,
+    status: "open",
+    createdAt: now,
+    updatedAt: now,
+  };
+
+  tickets.push(newTicket);
+
+// Return created ticket
+  return newTicket;
+};
