edeb2dda5c69e01beeca0ef2a9735cca569dd449
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-07 20:27:43 -0600

    Added Endpoint and controller for urgency calculation

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -4,13 +4,15 @@ import {
     getAllTicketsHandler,
     getTicketByIdHandler,
     updateTicketHandler,
-    deleteTicketHandler
+    deleteTicketHandler,
+    getTicketUrgencyHandler
 } from "../controllers/ticketController";
 
 const router = Router();
 
 router.post("/tickets", createTicketHandler);
 router.get("/tickets", getAllTicketsHandler);
+router.get("/tickets/:id/urgency", getTicketUrgencyHandler);
 router.get("/tickets/:id", getTicketByIdHandler);
 router.put("/tickets/:id", updateTicketHandler);
 router.delete("/tickets/:id", deleteTicketHandler);
