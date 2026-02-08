297a2be492978368030ac20574f99b9b2941e89f
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-07 20:04:44 -0600

    Added delete logic and controller

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -69,3 +69,13 @@ export const updateTicket = (
 
   return tickets[index];
 };
+
+export const deleteTicket = (id: number): boolean => {
+    
+  const index = tickets.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return false;
+  }
+  tickets.splice(index, 1);
+  return true;
+};
