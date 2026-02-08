84f88077719e949e248b356b01f4769af2db90fc
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-07 19:51:23 -0600

    Added sample ticket data

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { 
     createTicketHandler,
     getAllTicketsHandler,
-    getTicketByIdHandler 
+    getTicketByIdHandler,
+    updateTicketHandler
 } from "../controllers/ticketController";
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.post("/tickets", createTicketHandler);
 router.get("/tickets", getAllTicketsHandler);
 router.get("/tickets/:id", getTicketByIdHandler);
+router.put("/tickets/:id", updateTicketHandler);
 
 export default router;
