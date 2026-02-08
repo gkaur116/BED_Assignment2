c511a9f15ada36cf614f74dbb9bb68a986690396
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-02-07 18:35:14 -0600

    Added ticketController file

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -11,7 +11,6 @@ export interface Ticket {
   priority: TicketPriority;
   status: TicketStatus;
   createdAt: string;
-  updatedAt: string;
 }
 
 export const createTicket = (
@@ -23,13 +22,12 @@ export const createTicket = (
 
 
   const newTicket: Ticket = {
-    id: tickets.length + 1,
+    id: tickets.length > 0 ? Math.max(...tickets.map((t) => t.id)) + 1 : 1,
     title,
     description,
     priority,
     status: "open",
     createdAt: now,
-    updatedAt: now,
   };
 
   tickets.push(newTicket);
